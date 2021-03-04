7463a33d704cd5fd3c559d3c458bf52ccb1ffa4f
xiao-lix <lixiao900921@gmail.com>
2021-03-03 18:47:53 -0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import { AngularPlugin } from '@microsoft/applicationinsights-angularplugin-js';
 import { Router } from '@angular/router';
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private router: Router){
+  constructor(private router: Router, private ngZone: NgZone){
     const angularPlugin = new AngularPlugin();
     const appInsights = new ApplicationInsights({ config: {
-      instrumentationKey: '<YOUR_IKEY>',
+      instrumentationKey: '63969e7a-c5f7-4494-96f8-a33184d0fe30',
+      disableFlushOnBeforeUnload: true,
+      disableFlushOnUnload: true,
       extensions: [angularPlugin],
       extensionConfig: {
         [angularPlugin.identifier]: {
@@ -20,6 +23,11 @@ export class AppComponent {
         }
       }
     } });
+    this.ngZone.runOutsideAngular(() => {
+      appInsights.loadAppInsights();
+    });
+    // console.log(appInsights.config.connectionString);
+    // throw new Error('123');
     // appInsights.loadAppInsights();
   }
 }
